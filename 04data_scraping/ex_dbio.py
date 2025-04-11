d43ddc3d871565331e5ac51df270e3b015f14323
Peter-BoBo <sinbo109@gmail.com>
2025-04-11 18:14:12 +0900

    python_playstore_review_scraping

--- 04data_scraping/ex_dbio.py
+++ 04data_scraping/ex_dbio.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 import pymysql
 pymysql.install_as_MySQLdb()
 import pandas as pd
-from datetime import datetime
 import time
+# from datetime import datetime
 
 def dbconnect():
     engine = create_engine("mysql+pymysql://root:1234@localhost:3306/exchange_rate")
@@ -18,4 +18,14 @@ def to_ex_db(df):
     conn = dbconnect()
     time.sleep(1)
     df.to_sql(f'exchange_rate', con=conn,  if_exists="append", index=False)
+    conn.close()
+
+def to_ex_db_test(df):
+    """
+    
+    """
+    # Database 쿼리창 오픈
+    conn = dbconnect()
+    time.sleep(1)
+    df.to_sql(f'exchange_rate_today', con=conn,  if_exists="append", index=False)
     conn.close()
